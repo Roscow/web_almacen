8131960a2842f815265ea75c58b6968ca2568173
nicolas <nicolas.valdes@live.com>
2020-04-28 16:11:30 -0400

    agregados templates en views

---
+++ resources/views/base.blade.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, user-scalable=no">
+        <title>Console project</title>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+        <link href='//fonts.googleapis.com/css?family=Lobster&subset=latin,latin-ext' rel='stylesheet' type='text/css'>        
+    </head>
+    <body>
+            <div class="container">
+                <div class="row">
+                    <div class="title col-xs-12 col-lg-12">ALMACÉN</div>                    
+                </div>
+ 
+               
+                <div class="row foot_bar">
+                    <div class="col-xs-12 col-lg-12 foot_line">
+                        Desarrollado con Laravel- Sql Server -Portafolio 2020
+                    </div>
+                   
+                </div>
+            </div>
+        </body>
+</html>
