1f001df05c230f5eaf40f2896549254e36b7f1de
nicolas <nicolas.valdes@live.com>
2020-04-29 14:46:43 -0400

    agregado bootstrap a menu principal

--- resources/views/menu_principal.blade.php
+++ resources/views/menu_principal.blade.php
@@ -2,34 +2,27 @@
 
 
 @section('seccion')
-    <h1>MENU PRINCIPAL</h1>    
-        <ul>
-            <li>
-                <a href="{{ route('menu_usuario') }}" >Usuario</a>         
-            </li>  
 
-            <li>
-                <a href="{{ route('menu_cliente') }}">Cliente</a>
-            </li>
 
-            <li>
-                <a href="{{ route('menu_proveedor') }}">Proveedor</a>
-            </li>
-
-            <li>
-                <a href="{{ route('menu_ventas') }}">Venta</a>
-            </li>
-            <li>
-                <a href="{{ route('menu_stock') }}">Stock</a>
-            </li>
-            <li>
-                <a href="{{ route('menu_pedidos') }}">Pedidos</a>
-            </li>
-            <li>
-                <a href="{{ route('nuevo_reportes') }}">Reportes</a>                
-            </li>
-        </ul>    
-        
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">MENU PRINCIPAL</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">                    
+      <a class="nav-item nav-link" href="{{ route('menu_cliente') }}">Cliente</a>
+      <a class="nav-item nav-link" href="{{ route('menu_usuario') }}" >Usuario</a>         
+      <a class="nav-item nav-link" href="{{ route('menu_cliente') }}">Cliente</a>
+      <a class="nav-item nav-link" href="{{ route('menu_proveedor') }}">Proveedor</a>
+      <a class="nav-item nav-link" href="{{ route('menu_ventas') }}">Venta</a>
+      <a class="nav-item nav-link" href="{{ route('menu_stock') }}">Stock</a>
+      <a class="nav-item nav-link" href="{{ route('menu_pedidos') }}">Pedidos</a>
+      <a class="nav-item nav-link" href="{{ route('nuevo_reportes') }}">Reportes</a>                
+    </div>
+  </div>
+</nav>
+   
         <div class="container">
             @yield('seccion2')    
         </div>
