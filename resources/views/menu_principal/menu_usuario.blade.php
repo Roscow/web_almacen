7c65e5b5c336617f3eecbeaf6e5326aee7f53c5e
nicolas <nicolas.valdes@live.com>
2020-04-30 08:39:28 -0400

    agregado estilo a submenu usuario

--- resources/views/menu_principal/menu_usuario.blade.php
+++ resources/views/menu_principal/menu_usuario.blade.php
@@ -1,26 +1,29 @@
 @extends('menu_principal')
 
 
-@section('seccion2')
-   
-<h1>MENU USUARIO!</h1>    
-        <ul>
-            <li>
-                <a href="{{ route('usuario_crear') }}" >Agregar</a>         
-            </li>  
+@section('seccion2')           
+        <!-- la seccion media de la pagina -->
+        <div class="row">
 
-            <li>
-                <a href="{{ route('usuario_editar') }}">Editar</a>
-            </li>
+            <!-- el siderbar de los botones  -->
+          <div class="col-2">
+                <p>MENU USUARIO</p>                             
+                <div class="btn-group-vertical" role="link" aria-label="Button group with nested dropdown">
+                    <a class="btn btn-primary" href="{{ route('usuario_crear') }}" role="button">Agregar</a>
+                    <a class="btn btn-primary" href="{{ route('usuario_editar') }}" role="button">Editar</a>
+                    <a class="btn btn-primary" href="{{ route('usuario_eliminar') }}" role="button">Eliminar</a>
+                    <a class="btn btn-primary" href="{{ route('usuario_ver_todos') }}" role="button">Ver Todos</a>
+                </div>             
+          </div>                          
 
-            <li>
-                <a href="{{ route('usuario_eliminar') }}">Eliminar</a>
-            </li>
+            <!-- el espacio de contenido-->
+          <div class="col-10 border border-primary">
+            <p>contenido</p>   
 
-            <li>
-                <a href="{{ route('usuario_ver_todos') }}">Ver Todos</a>
-            </li>                                   
-        </ul>   
+          </div>
 
-         
+
+
+      <!--para crear botones laterales-->
+    </div>
 @endsection 
