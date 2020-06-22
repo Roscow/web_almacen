fe30492c4be78abf3a044b8ef5cb8864b6abe1a7
makarena.212@gmail.com <makarena.212@gmail.com>
2020-06-21 23:27:45 -0400

    Modificacion de Menu Superior

--- resources/views/menu_principal/menu_ventas.blade.php
+++ resources/views/menu_principal/menu_ventas.blade.php
@@ -6,15 +6,6 @@
 
     <!-- la seccion media de la pagina -->
   <div class="row">
-            <!-- el siderbar de los botones  -->
-          <div class="col-2">
-                <p>MENU VENTAS</p>
-                <div class="btn-group-vertical" role="link" aria-label="Button group with nested dropdown">
-                    <a class="btn btn-primary" href="{{ route('ventas_agregar') }}" role="button">Nueva venta</a>
-                    <a class="btn btn-primary" href="{{ route('ventas_anular') }}" role="button">Anular venta</a>
-                    <a class="btn btn-primary" href="{{ route('ventas_ver') }}" role="button">Ver ventas</a>
-                </div>
-          </div>
             <!-- el espacio de contenido-->
           <div class="col-10 border border-primary">
 
