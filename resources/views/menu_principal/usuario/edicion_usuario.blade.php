9d159bee8fa156a7028458a9fd8e42860b1506cb
makarena.212@gmail.com <makarena.212@gmail.com>
2020-07-07 18:36:04 -0400

    Modificacion de Observaciones de Pruebas.

--- resources/views/menu_principal/usuario/edicion_usuario.blade.php
+++ resources/views/menu_principal/usuario/edicion_usuario.blade.php
@@ -12,63 +12,64 @@
         <div class="form-group col-md-6">
             <label for="inputEmail4">Correo</label>
             <input type="email" class="form-control" id="correo" name="correo" value="{{$usuario[0]->correo}}" placeholder="ejemplo@correo.cl" required>
-        </div>   
+        </div>
 
         <div class="form-group col-md-6">
             <label for="inputAddress">Fecha de nacimiento</label>
             <input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" value="{{$year}}-{{$month}}-{{$day}}" required >
-        </div>          
+        </div>
     </div>
 
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="inputAddress">Primer Nombre</label>
-            <input type="text" class="form-control" id="nombre1" name="nombre1" value="{{$usuario[0]->nombre1}}" placeholder="Juan" 
+            <input type="text" class="form-control" id="nombre1" name="nombre1" value="{{$usuario[0]->nombre1}}" placeholder="Juan"
             onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)" required>
-        </div> 
+        </div>
 
         <div class="form-group col-md-6">
             <label for="inputAddress">Segundo Nombre</label>
-            <input type="text" class="form-control" id="nombre2" name="nombre2" value="{{$usuario[0]->nombre2}}" placeholder="Andres" 
+            <input type="text" class="form-control" id="nombre2" name="nombre2" value="{{$usuario[0]->nombre2}}" placeholder="Andres"
             onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)" required >
-        </div>    
+        </div>
     </div>
 
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="inputAddress">Primer Apellido</label>
-            <input type="text" class="form-control" id="apellido1" name="apellido1" value="{{$usuario[0]->apellido1}}" placeholder="Valdes" 
+            <input type="text" class="form-control" id="apellido1" name="apellido1" value="{{$usuario[0]->apellido1}}" placeholder="Valdes"
             onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)" required>
-        </div> 
+        </div>
 
         <div class="form-group col-md-6">
             <label for="inputAddress">Segundo Apellido</label>
-            <input type="text" class="form-control" id="apellido2" name="apellido2" value="{{$usuario[0]->apellido2}}" placeholder="Barria" 
+            <input type="text" class="form-control" id="apellido2" name="apellido2" value="{{$usuario[0]->apellido2}}" placeholder="Barria"
             onkeypress="return (event.charCode > 64 && event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)" required>
-        </div>    
+        </div>
     </div>
 
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="inputAddress">Numero</label>
             <input type="text" class="form-control" id="numero" name="numero" value="{{$usuario[0]->telefono}}" placeholder="1231231"  maxlength="9" onkeypress="return soloNumeros(event)" onpaste="return false" required>
-        </div>    
+        </div>
 
-         
+
     </div>
 
-    <div class="form-row">    
+    <div class="form-row">
         <div class="form-check">
-            @if($usuario[0]->id_tipo_user === 1 )
+            @if($usuario[0]->id_tipo_user === 0 )
                 <input class="form-check-input" type="checkbox" id="id_tipo_user" name="id_tipo_user" checked>
             @else
                 <input class="form-check-input" type="checkbox" id="id_tipo_user" name="id_tipo_user">
-            @endif   
+            @endif
             <label class="form-check-label" for="gridCheck">
                 Usuario Administrador
             </label>
-        </div>  
+        </div>
     </div>
+    <br>
     <button type="submit" class="btn btn-primary">Guardar</button>
 </form>
 @endsection
