7c6679665602c7fcc34c6a961116d7ab6f603ff1
nicolas <nicolas.valdes@live.com>
2020-07-08 18:21:06 -0400

    avance reporte

--- resources/views/menu_principal/reporte_ver.blade.php
+++ resources/views/menu_principal/reporte_ver.blade.php
@@ -8,26 +8,25 @@
         foreach($articulosPorVencer as $arts){
             foreach($productos as $prods){
                 if($prods->codigo_producto == $arts->id_producto){
-                    $var2= $var2 . $prods->nombre . "<br>";
+                    $var2= $var2 . $prods->nombre ." ".$arts->fecha_vencimiento. "<br>";
                 }                
             }            
         }
 
 
 
-    $var1 = "<h1>Almacen los yuyitos</h1>" . 
-    "<h5>Reporte</h5>".
-    "Periodo de analisis: $month de $year<br>".
-    "<br>N° ventas en el periodo: ". $cantidadVentas .
-    "<br>Productos mas vendidos:".
-    "<br>Usuario que mas ventas realizo:". 
-    "<br>Proveedor con mas pedidos: ". 
-    "<br>Articulos vencidos en el periodo".
-    "<br>--------------------------------------------".
-    "<br>Articulos que vencen en el mes actual:". $var2.
-    "<br>Productos con stock critico".
-    "<br>";
-    echo  $var1;
+        $var1 = "<h1>Almacen los yuyitos</h1>" . 
+        "<h5>Reporte</h5>".
+        "Periodo de analisis: $month de $year<br>".
+        "<br>N° ventas en el periodo: ". $cantidadVentas .
+        "<br>Productos mas vendidos:".
+        "<br>Usuario que mas ventas realizo:". 
+        "<br>Proveedor con mas pedidos: ". 
+        "<br>--------------------------------------------".
+        "<br>Articulos que vencen en el mes actual:". $var2.
+        "<br>Productos con stock critico".
+        "<br>";
+        echo  $var1;
     ?>
 
     <form action="{{route('prueba2')}}" method="POST">
